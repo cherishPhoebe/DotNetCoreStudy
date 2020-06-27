b976eedc93527acb7efb6ae538ad10e8576295dc
cherishPhoebe <cherish1224@outlook.com>
2020-06-27 22:28:56 +0800

    Startup Demo

--- Samples/ConfigurationDemo/Program.cs
+++ Samples/ConfigurationDemo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace ConfigurationDemo
 {
@@ -6,8 +8,25 @@ namespace ConfigurationDemo
     {
         static void Main(string[] args)
         {
+            var build = new ConfigurationBuilder();
+
+            build.AddInMemoryCollection(new Dictionary<string, string>()
+            {
+                { "key1","value1"},
+                { "key2","value2"},
+                { "key3","value3"},
+                { "key4","value4"},
+                { "key5","value5"}
+            });
+
+            IConfigurationRoot configurationRoot = build.Build();
+
+            Console.WriteLine(configurationRoot["key1"]);
+            Console.WriteLine(configurationRoot["key2"]);
+
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
- 
