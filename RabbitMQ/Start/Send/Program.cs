2b44d32850352612425f37c9a9326a5e20c97a12
cherishPhoebe <cherish1224@outlook.com>
2021-12-29 20:50:25 +0800

    add rabbitmq demo

---
+++ RabbitMQ/Start/Send/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello World";
+                    var body
+
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
