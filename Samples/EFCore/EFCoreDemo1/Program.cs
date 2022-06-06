80355188a7bfacd479d54d955752d5f90171ee27
cherishPhoebe <cherish1224@outlook.com>
2022-06-06 23:47:16 +0800

    增加数据操作示例

--- Samples/EFCore/EFCoreDemo1/Program.cs
+++ Samples/EFCore/EFCoreDemo1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EFCoreDemo1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            // 注入DBContext 到容器
+            IServiceCollection service = new ServiceCollection();
+            service.AddDbContext<MyDbContext>();
+
+            using (var sp = service.BuildServiceProvider())
+            {
+                using (var dbContext = sp.GetService<MyDbContext>())
+                {
+                    var book = new Book();
+                    book.Title = "yangzhongkejiangdotnetcore";
+                    book.AuthorName = "yzk";
+                    book.Price = 0;
+                    book.PubTime = DateTime.Now;
+
+                    await dbContext.Books.AddAsync(book);
+
+                    await dbContext.SaveChangesAsync();
+                }
+
+            }
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
