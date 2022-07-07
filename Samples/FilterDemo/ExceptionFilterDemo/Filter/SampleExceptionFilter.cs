cf54a0b9df156f4fc3b3fbe949e9ffb460e1c79f
midnight <384998447@qq.com>
2022-07-07 18:32:13 +0800

    add exceptionfilter demo

---
+++ Samples/FilterDemo/ExceptionFilterDemo/Filter/SampleExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExceptionFilterDemo.Filter
+{
+    public class SampleExceptionFilter : IAsyncExceptionFilter
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public SampleExceptionFilter(IHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public Task OnExceptionAsync(ExceptionContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
