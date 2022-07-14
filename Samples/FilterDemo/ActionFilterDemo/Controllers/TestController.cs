c169cf4fcfcc5b60f07e134d0b538d614db6652a
midnight <384998447@qq.com>
2022-07-14 18:12:38 +0800

    edit

--- Samples/FilterDemo/ActionFilterDemo/Controllers/TestController.cs
+++ Samples/FilterDemo/ActionFilterDemo/Controllers/TestController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ActionFilterDemo.Attributes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ActionFilterDemo.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
+    [NoLogAttribute]
     public class TestController : ControllerBase
     {
+        [HttpGet]
+        [NoLogAttribute]
         public IActionResult Index()
         {
             return Ok();
--- Samples/FilterDemo/ActionFilterDemo/Filters/SampleActionFilter.cs
+++ Samples/FilterDemo/ActionFilterDemo/Filters/SampleActionFilter.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using ActionFilterDemo.Attributes;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ActionFilterDemo.Filters
 {
     public class SampleActionFilter : IAsyncActionFilter
     {
+        private ILogger<SampleActionFilter> _logger;
+
+        public SampleActionFilter(ILogger<SampleActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             // action 方法执行前执行的代码
-
-            await next();
+            var isNoLog = false;
+            if (context.ActionDescriptor is ControllerActionDescriptor) {
+                var controllerActionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
+                isNoLog = controllerActionDescriptor.MethodInfo.IsDefined(typeof(NoLogAttribute),true);
+            }
+            if (isNoLog)
+            {
+                await next();
+                return;
+            }
 
             // action 方法执行后执行的代码
 
